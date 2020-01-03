d022490004ea1433278dd8868eee3ddcdc3478ba
Navanath <navanathb@bsf.io>
2020-01-03 19:03:14 +0530

    2.0 options usability WIP

--- classes/customizer-panels-and-sections.php
+++ classes/customizer-panels-and-sections.php
@@ -3,38 +3,93 @@
  * Astra Slider - Panels & Sections
  *
  * @package Home Page Slider for Astra Theme
- * @since 1.0.0
+ * @author      Brainstorm Force
+ * @copyright   Copyright (c) 2020, Brainstorm Force
+ * @link        https://www.brainstormforce.com
+ * @since       1.0.0
  */
 
-/**
- * Layout Panel
- */
+// Block direct access to the file.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-$wp_customize->add_panel(
-	'panel-astra-slider', array(
-		'priority' => 20,
-		'title'    => __( 'Astra Slider', 'astra-slider' ),
-	)
-);
+// Bail if Customizer config base class does not exist.
+if ( ! class_exists( 'Astra_Customizer_Config_Base' ) ) {
+	return;
+}
 
-$ast_hompage_slide_count = apply_filters( 'ast_hompage_slide_count', 3 );
+if ( ! class_exists( 'Astra_Slider_Panels_Configs' ) ) {
 
-if( $ast_hompage_slide_count ) {
-	for( $base = 1; $base <= $ast_hompage_slide_count; $base++ ) {
-		$wp_customize->add_section(
-			'section-astra-slider-'. $base .'-contents', array(
-				'priority' => 5 + $base,
-				'title'    => __( 'Slide ' . $base .' ' , 'astra-slider' ),
-				'panel'    => 'panel-astra-slider',
-			)
-		);
+	/**
+	 * Register Astra Slider Customizer Configurations.
+	 */
+	class Astra_Slider_Panels_Configs extends Astra_Customizer_Config_Base {
+
+		/**
+		 * Register Astra Slider Customizer Configurations.
+		 *
+		 * @param Array                $configurations Astra Customizer Configurations.
+		 * @param WP_Customize_Manager $wp_customize instance of WP_Customize_Manager.
+		 * @since 1.4.3
+		 * @return Array Astra Customizer Configurations with updated configurations.
+		 */
+		public function register_configuration( $configurations, $wp_customize ) {
+
+			$_config = array(
+
+				/*
+				 * Slider Panel Section
+				 *
+				 * @since 1.0.0
+				 */
+				array(
+					'name'     => 'panel-astra-slider',
+					'type'     => 'panel',
+					'priority' => 20,
+					'title'    => __( 'Astra Slider', 'astra-slider' ),
+				),
+
+				array(
+					'name'     => 'section-astra-slides',
+					'title'    => __( 'Slides', 'astra-slider' ),
+					'panel'    => 'panel-astra-slider',
+					'priority' => 5,
+					'type'     => 'section',
+				),
+
+				array(
+					'name'     => 'section-slide-design',
+					'title'    => __( 'Design', 'astra-slider' ),
+					'panel'    => 'panel-astra-slider',
+					'priority' => 10,
+					'type'     => 'section',
+				),
+			);
+
+			$ast_hompage_slide_count = apply_filters( 'ast_hompage_slide_count', 3 );
+
+			if( $ast_hompage_slide_count ) {
+
+				for( $base = 1; $base <= $ast_hompage_slide_count; $base++ ) {
+
+					$ast_slide = array(
+						array(
+							'name'     => 'section-astra-slide-'. $base .'-contents',
+							'title'    => __( 'Slide ' . $base .' ' , 'astra-slider' ),
+							'section'  => 'section-astra-slides',
+							'priority' => 5 + $base,
+							'type'     => 'section',
+						),
+					);
+
+					$_config = array_merge( $_config, $ast_slide );
+				}
+			}
+
+			return array_merge( $configurations, $_config );
+		}
 	}
 }
 
-$wp_customize->add_section(
-	'section-astra-slider-styles', array(
-		'priority' => 100,
-		'title'    => __( 'Style', 'astra-slider' ),
-		'panel'    => 'panel-astra-slider',
-	)
-);
+new Astra_Slider_Panels_Configs;
--- classes/sections/section-slider.php
+++ classes/sections/section-slider.php
@@ -1,81 +1,111 @@
 <?php
 /**
- * Home Page Banner options for Astra.
+ * Astra Slider Options for our theme.
  *
- * @package Home Page Banner for Astra Theme
- * @since 1.0.0
+ * @package     Astra Slider
+ * @author      Brainstorm Force
+ * @copyright   Copyright (c) 2019, Brainstorm Force
+ * @link        https://www.brainstormforce.com
+ * @since       1.0.0
  */
 
+// Block direct access to the file.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-$ast_hompage_slide_count = apply_filters( 'ast_hompage_slide_count', 3 );
-
-if( $ast_hompage_slide_count ) {
-
-	for( $base = 1; $base <= $ast_hompage_slide_count; $base++ ) {
-
-		/**
-		 * Option: Retina logo selector
-		 */
-		$wp_customize->add_setting(
-			ASTRA_THEME_SETTINGS . '[astra-slider-banner-'. $base .'-image]', array(
-				'default'           => astra_get_option( 'astra-slider-banner-'. $base .'-image' ),
-				'type'              => 'option',
-				'sanitize_callback' => 'esc_url_raw',
-			)
-		);
-
-		$wp_customize->add_control(
-			new WP_Customize_Image_Control(
-				$wp_customize, ASTRA_THEME_SETTINGS . '[astra-slider-banner-'. $base .'-image]', array(
-					'section'        => 'section-astra-slider-'. $base .'-contents',
-					'priority'       => 5,
-					'label'          => __( 'Banner Image', 'astra-slider' ),
-					'library_filter' => array( 'gif', 'jpg', 'jpeg', 'png', 'ico' ),
-				)
-			)
-		);
-
-		/**
-		 * Option: Add Heading
-		 */
-		$wp_customize->add_setting(
-			ASTRA_THEME_SETTINGS . '[astra-slider-banner-'. $base .'-heading]', array(
-				'default'           => astra_get_option( 'astra-slider-banner-'. $base .'-heading' ),
-				'type'              => 'option',
-				'sanitize_callback' => 'sanitize_text_field',
-			)
-		);
-
-		$wp_customize->add_control(
-			ASTRA_THEME_SETTINGS . '[astra-slider-banner-'. $base .'-heading]', array(
-				'type'     => 'text',
-				'section'  => 'section-astra-slider-'. $base .'-contents',
-				'label'    => __( 'Banner Heading', 'astra-slider' ),
-				'priority' => 6,
-			)
-		);
-
-		/**
-		 * Option: Add Sub Heading
-		 */
-		$wp_customize->add_setting(
-			ASTRA_THEME_SETTINGS . '[astra-slider-banner-'. $base .'-subheading]', array(
-				'default'           => astra_get_option( 'astra-slider-banner-'. $base .'-subheading' ),
-				'type'              => 'option',
-				'sanitize_callback' => 'sanitize_text_field',
-			)
-		);
-
-		$wp_customize->add_control(
-			ASTRA_THEME_SETTINGS . '[astra-slider-banner-'. $base .'-subheading]', array(
-				'type'     => 'text',
-				'section'  => 'section-astra-slider-'. $base .'-contents',
-				'label'    => __( 'Banner Subheading', 'astra-slider' ),
-				'priority' => 6,
-			)
-		);
-	}
+// Bail if Customizer config base class does not exist.
+if ( ! class_exists( 'Astra_Customizer_Config_Base' ) ) {
+	return;
 }
+
+if ( ! class_exists( 'Astra_Slide_Configs' ) ) {
+
+	/**
+	 * Register Astra Slider Customizer Configurations.
+	 */
+	class Astra_Slide_Configs extends Astra_Customizer_Config_Base {
+
+		/**
+		 * Register Astra Slider Customizer Configurations.
+		 *
+		 * @param Array                $configurations Astra Customizer Configurations.
+		 * @param WP_Customize_Manager $wp_customize instance of WP_Customize_Manager.
+		 * @since 1.4.3
+		 * @return Array Astra Customizer Configurations with updated configurations.
+		 */
+		public function register_configuration( $configurations, $wp_customize ) {
+
+			$ast_hompage_slide_count = apply_filters( 'ast_hompage_slide_count', 3 );
+
+			$_config = array();
+
+			if( $ast_hompage_slide_count ) {
+
+				for( $base = 1; $base <= $ast_hompage_slide_count; $base++ ) {
+
+					$_config = array(
+
+						/**
+						 * Option: Slide Above Divider
+						 */
+						 array(
+							'name'            => 'ast-slider-divider-section-slide-' . $base . '-header',
+							'type'            => 'control',
+							'control'         => 'ast-heading',
+							'section'         => 'section-astra-slide-'. $base .'-contents',
+							'title'           => __( 'Slide ' . $base . ' ', 'astra-slider' ),
+							'priority'        => 5,
+						),
+
+						/**
+						 * Option: Slide image selector
+						 */
+						array(
+							'name'           => 'astra-slider-banner-'. $base .'-image',
+							'default'        => astra_get_option( 'astra-slider-banner-'. $base .'-image' ),
+							'type'           => 'control',
+							'control'        => 'image',
+							'section'        => 'section-astra-slider-'. $base .'-contents',
+							'priority'       => 10,
+							'title'          => __( 'Sticky Logo', 'astra-slider' ),
+							'library_filter' => array( 'gif', 'jpg', 'jpeg', 'png', 'ico' ),
+						),
+
+						/**
+						 * Option: Slide Heading
+						 */
+						array(
+							'name'     => 'astra-slider-banner-'. $base .'-heading',
+							'default'  => astra_get_option( 'astra-slider-banner-'. $base .'-heading' ),
+							'type'     => 'control',
+							'section'  => 'section-astra-slider-'. $base .'-contents',
+							'priority' => 15,
+							'title'    => __( 'Slide Heading', 'astra-slider' ),
+							'control'  => 'text',
+						),
+
+						/**
+						 * Option: Slide Description
+						 */
+						array(
+							'name'      => 'astra-slider-banner-'. $base .'-subheading',
+							'default'   => astra_get_option( 'astra-slider-banner-'. $base .'-subheading' ),
+							'type'      => 'control',
+							'control'   => 'textarea',
+							'section'   => 'section-astra-slider-'. $base .'-contents',
+							'priority'  => 20,
+							'description' => __( 'Custom Text / HTML allowed.', 'astra-slider' ),
+							'title'     => __( 'Slide Description', 'astra' ),
+						),
+					);
+				}
+			}
+
+			return array_merge( $configurations, $_config );
+		}
+	}
+
+}
+	
+new Astra_Slide_Configs;
